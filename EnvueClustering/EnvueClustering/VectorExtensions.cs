a5f38b43ee016f8284cb5214613bddad14bf7287
Minibrams <anders@brams.dk>
2019-03-18 22:58:01 +0100

    Added docs to vector extensions

--- EnvueClustering/EnvueClustering/VectorExtensions.cs
+++ EnvueClustering/EnvueClustering/VectorExtensions.cs
@@ -7,6 +7,11 @@ namespace EnvueClustering
 {
     public static class VectorExtensions
     {
+        /// <summary>
+        /// Returns the element-wise sum of a vector.
+        /// </summary>
+        /// <param name="vector">An array of floating point values.</param>
+        /// <exception cref="ArgumentException">An empty vector will yield an exception.</exception>
         public static float Sum(this float[] vector)
         {
             if (vector.Length == 0)
@@ -15,6 +20,12 @@ namespace EnvueClustering
             return Enumerable.Sum(vector);
         }
 
+        /// <summary>
+        /// Returns the euclidean length of a vector, i.e. the square root of the sum of squared elements
+        /// in the vector.
+        /// </summary>
+        /// <param name="vector">An array of floating point values.</param>
+        /// <exception cref="ArgumentException">An empty array will yield an exception.</exception>
         public static float EuclideanLength(this float[] vector)
         {
             if (vector.Length == 0)
@@ -24,6 +35,9 @@ namespace EnvueClustering
             return (float) Math.Sqrt(squaredSum);
         }
 
+        /// <summary>
+        /// Returns a pretty-printed representation of a vector.
+        /// </summary>
         public static string Pretty<T>(this T[] vector)
         {
             var stringBuilder = new StringBuilder();
@@ -33,6 +47,10 @@ namespace EnvueClustering
             return stringBuilder.ToString();
         }
 
+        /// <summary>
+        /// Returns a pretty-printed representation of a
+        /// multi-dimensional array.
+        /// </summary>
         public static string Pretty<T>(this T[][] vectorSeries)
         {
             var stringBuilder = new StringBuilder();
@@ -41,18 +59,15 @@ namespace EnvueClustering
 
             return stringBuilder.ToString();
         }
-
-        public static T[][] Window<T>(this T[] arr, int windowSize, float overlap)
-        {
-            var windows = new List<T[]>();
-            for (int i = 0; i < arr.Length; i += (int) (windowSize * overlap))
-            {
-                windows.Add(arr.Skip(i).Take(windowSize).ToArray());
-            }
-
-            return windows.ToArray();
-        }
         
+        /// <summary>
+        /// Returns a slice (or sub-array) from a vector.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="index">The index from which the slice will start.</param>
+        /// <param name="length">The length of the slice.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T[] Slice<T>(this T[] source, int index, int length)
         {       
             var slice = new T[length];
@@ -65,9 +80,15 @@ namespace EnvueClustering
             return slice;
         }
 
+        /// <summary>
+        /// Returns an enumerable range of integers. 
+        /// </summary>
+        /// <param name="max">The end (exclusive) of the range.</param>
+        /// <param name="start">The start (inclusive) of the range (0 by default).</param>
+        /// <returns>A range [start, max - 1]</returns>
         public static IEnumerable<int> Range(this int max, int start = 0)
         {
-            return Enumerable.Range(start, max);
+            return Enumerable.Range(start, max - 1);
         }
     }
 }

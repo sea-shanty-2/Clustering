31f9d8bd8bc7bdf1c00fcfc8965933af0cca51b1
minibrams <anders@brams.dk>
2019-03-28 16:00:12 +0100

    Added swagger to the APIs

--- EnvueClustering/EnvueClusteringAPI/Startup.cs
+++ EnvueClustering/EnvueClusteringAPI/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using EnvueClustering;
 using EnvueClustering.Data;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace EnvueClusteringAPI
 {
@@ -28,7 +31,11 @@ namespace EnvueClusteringAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "Envue Clustering", Version = "v1"});
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +52,12 @@ namespace EnvueClusteringAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("swagger/v1/swagger.json", "Envue Clustering v1");
+                c.RoutePrefix = "";
+            });
         }
     }
 }

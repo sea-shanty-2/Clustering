11a0a7445f8030676bed47535f709c711d0fb0b1
minibrams <anders@brams.dk>
2019-03-28 15:06:48 +0100

    Implemented ITransformable<Streamer> for Streamer class

--- EnvueClustering/EnvueClusteringAPI/Models/Streamer.cs
+++ EnvueClustering/EnvueClusteringAPI/Models/Streamer.cs
@@ -1,23 +1,70 @@
 
 using System;
+using EnvueClustering.ClusteringBase;
 
 namespace EnvueClusteringAPI.Models
 {
     /// <summary>
     /// Model class for containing information about streamers.
     /// </summary>
-    public class Streamer
+    public class Streamer : ITransformable<Streamer>
     {
-        private float _lon, _lat;
         private int _started;
-        private Guid _id;
+
+        public float Longitude { get; }
+
+        public float Latitude { get; }
+
+        public Guid Id { get; }
 
         public Streamer(float lon, float lat, int timestamp, Guid id)
         {
-            _lon = lon;
-            _lat = lat;
+            Longitude = lon;
+            Latitude = lat;
             _started = timestamp;
-            _id = id;
+            Id = id;
+        }
+
+        public Streamer Scale(float scalar)
+        {
+            return new Streamer(Longitude * scalar, Latitude * scalar, TimeStamp, Id);
+        }
+
+        public Streamer Divide(float scalar)
+        {
+            return new Streamer(Longitude / scalar, Latitude / scalar, TimeStamp, Id);
+        }
+
+        public Streamer Add(Streamer other)
+        {
+            return new Streamer(Longitude + other.Longitude, Latitude + other.Latitude, TimeStamp, Id);
+        }
+
+        public Streamer Subtract(Streamer other)
+        {
+            return new Streamer(Longitude - other.Longitude, Latitude - other.Latitude, TimeStamp, Id);
+        }
+
+        public Streamer Pow(int power)
+        {
+            return new Streamer((float) Math.Pow(Longitude, 2), (float)Math.Pow(Longitude, 2), TimeStamp, Id);
+        }
+
+        public Streamer Sqrt()
+        {
+            return new Streamer((float) Math.Sqrt(Longitude), (float)Math.Sqrt(Longitude), TimeStamp, Id);
+        }
+
+        public float Size()
+        {
+            // This is only used for CF1 and CF2 calculations in the CMC classes, and we don't use those.
+            throw new NotImplementedException();
+        }
+
+        public int TimeStamp
+        {
+            get => _started;
+            set => _started = value;
         }
     }
 }

2434f9b7f83c6bdd3184038a1b8f6c85a5deb283
Matias Jensen <sejemus@yahoo.dk>
2019-05-01 14:46:43 +0200

    Renamed build task to test

--- EnvueClustering/EnvueClustering.Tests/UnitTests/DenstreamTests.cs
+++ EnvueClustering/EnvueClustering.Tests/UnitTests/DenstreamTests.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using EnvueClustering.ClusteringBase;
-using EnvueClustering.Exceptions;
 using EnvueClustering.TimelessDenStream;
 using EnvueClusteringAPI.Models;
 using NUnit.Framework;

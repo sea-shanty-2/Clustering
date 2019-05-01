b68b13ed1ebf7b3625d195786a265753a4cc31a9
Matias Jensen <sejemus@yahoo.dk>
2019-05-01 14:39:32 +0200

    Updated tests

--- EnvueClustering/EnvueClustering.Tests/UnitTests/DenstreamTests.cs
+++ EnvueClustering/EnvueClustering.Tests/UnitTests/DenstreamTests.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using EnvueClustering.ClusteringBase;
 using EnvueClustering.Exceptions;
 using EnvueClustering.TimelessDenStream;
@@ -20,17 +22,34 @@ namespace EnvueClustering.Tests
                 Similarity.Haversine, 
                 Similarity.Haversine);
         }
-
+        
         [Test]
         public void AddEnumerable_SameTwoPoints_OneMicroCluster()
         {
-            List<Streamer> streamers = new List<Streamer>();
-            streamers.Add(new Streamer(10, 20, new float[]{1, 0, 1}, 0, "Test"));
-            streamers.Add(new Streamer(10, 22, new float[]{1, 0, 1}, 1, "Test2"));
-            
+            List<Streamer> streamers = new List<Streamer>
+            {
+                new Streamer(10, 20, new float[] {1, 0, 1}, 0, "Test"),
+                new Streamer(10, 20, new float[] {1, 0, 1}, 0, "Test")
+            };
+
             _denStream.Add(streamers);
-            _denStream.Cluster();
             
+            Thread.Sleep(1000);
+            Assert.AreEqual(1, _denStream.MicroClusters.Count);
+        }
+
+        [Test]
+        public void AddEnumerable_CloseTwoPoints_OneMicroCluster()
+        {
+            List<Streamer> streamers = new List<Streamer>
+            {
+                new Streamer(10, 20, new float[] {1, 0, 1}, 0, "Test"),
+                new Streamer(10, 22, new float[] {1, 0, 1}, 1, "Test2")
+            };
+
+            _denStream.Add(streamers);
+            
+            Thread.Sleep(1000);
             Assert.AreEqual(1, _denStream.MicroClusters.Count);
         }
         
@@ -42,29 +61,28 @@ namespace EnvueClustering.Tests
             streamers.Add(new Streamer(0, 100, new float[]{0, 1, 1}, 1, "Test2"));
             
             _denStream.Add(streamers);
-            _denStream.Cluster();
             
+            Thread.Sleep(1000);
             Assert.AreEqual(2, _denStream.MicroClusters.Count);
         }
         
         [Test]
-        public void AddEnumerable_AddOnePoint_ArgumentExceptionThrown()
+        public void AddEnumerable_AddOnePoint_OneMicroCluster()
         {
-            List<Streamer> streamers = new List<Streamer>();
-            streamers.Add(new Streamer(10, 20, new float[]{1, 0, 1}, 0, "Test"));
+            List<Streamer> streamers = new List<Streamer> {new Streamer(10, 20, new float[] {1, 0, 1}, 0, "Test")};
             
             _denStream.Add(streamers);
 
-            EnvueArgumentException exception = Assert.Throws<EnvueArgumentException>(() => _denStream.Cluster());
-            Assert.AreEqual("No micro clusters available, aborting DBSCAN clustering.", exception.Message);
+            Thread.Sleep(1000);
+            Assert.AreEqual(1, _denStream.MicroClusters.Count);
         }
 
         [Test]
-        public void AddSingle_AddOnePoint_OneCluster()
+        public void AddSingle_AddOnePoint_OneMicroCluster()
         {
             _denStream.Add(new Streamer(10, 20, new float[]{1, 0, 1}, 0, "Test"));
-            _denStream.Cluster();
             
+            Thread.Sleep(1000);
             Assert.AreEqual(1, _denStream.MicroClusters.Count);
         }
     }

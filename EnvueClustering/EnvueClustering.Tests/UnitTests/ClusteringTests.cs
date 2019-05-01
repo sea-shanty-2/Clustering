920aabf9b1ba61307dd87450d943c5f15f523035
minibrams <anders@brams.dk>
2019-05-01 12:57:32 +0200

    Added tests for number of clusters given same points

--- EnvueClustering/EnvueClustering.Tests/UnitTests/ClusteringTests.cs
+++ EnvueClustering/EnvueClustering.Tests/UnitTests/ClusteringTests.cs
@@ -42,5 +42,37 @@ namespace EnvueClustering.Tests
             var clusters = ds.Cluster();
             Assert.That(clusters, Is.Not.Empty);
         }
+
+        [Test]
+        public void Cluster_SameTwoPoints_OneCluster()
+        {
+            var ds = new TimelessDenStream<Streamer>(Similarity.Haversine, Similarity.Haversine);
+            // [{"longitude":10.0,"latitude":20.0,"streamDescription":[0.0,1.0,0.0],"id":"Test","timeStamp":0}]
+            var streamers = new[]
+            {
+                new Streamer(10.0f, 20.0f, new [] {0.0f, 1.0f, 0.0f}, 0, "Test"),
+                new Streamer(10.0f, 20.0f, new [] {0.0f, 1.0f, 0.0f}, 0, "Test")
+            };
+            
+            ds.Add(streamers);
+            var clusters = ds.Cluster();
+            Assert.That(clusters, Has.Exactly(1).Items);
+        }
+        
+        [Test]
+        public void Cluster_SameTwoPoints_OneMicroCluster()
+        {
+            var ds = new TimelessDenStream<Streamer>(Similarity.Haversine, Similarity.Haversine);
+            // [{"longitude":10.0,"latitude":20.0,"streamDescription":[0.0,1.0,0.0],"id":"Test","timeStamp":0}]
+            var streamers = new[]
+            {
+                new Streamer(10.0f, 20.0f, new [] {0.0f, 1.0f, 0.0f}, 0, "Test"),
+                new Streamer(10.0f, 20.0f, new [] {0.0f, 1.0f, 0.0f}, 0, "Test")
+            };
+            
+            ds.Add(streamers);
+            var mcs = ds.MicroClusters;
+            Assert.That(mcs, Has.Exactly(1).Items);
+        }
     }
 }
